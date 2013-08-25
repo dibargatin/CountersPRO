d613feb13984edb807e98f714c2ec9ca869d72a6
Di Bargatin <dibargatin@gmail.com>
2013-08-25 14:15:23 +0400

    Релиз №7 версия 1.0.6

--- src/com/blogspot/dibargatin/counterspro/database/IndicationsExpandableListAdapter.java
+++ src/com/blogspot/dibargatin/counterspro/database/IndicationsExpandableListAdapter.java
@@ -592,9 +592,9 @@ public class IndicationsExpandableListAdapter extends BaseExpandableListAdapter 
             StringBuilder sb = new StringBuilder();
             
             c.setTimeInMillis(maxTime);
-            c.set(Calendar.HOUR_OF_DAY, 0);
-            c.set(Calendar.MINUTE, 0);
-            c.set(Calendar.SECOND, 0);
+            c.set(Calendar.HOUR_OF_DAY, 23);
+            c.set(Calendar.MINUTE, 59);
+            c.set(Calendar.SECOND, 59);
             c.set(Calendar.MILLISECOND, 0);
             c.set(Calendar.DAY_OF_WEEK, c.getFirstDayOfWeek());
             c.add(Calendar.DATE, 6);      
